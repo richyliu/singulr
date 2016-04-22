e44b335ffe2b8047e27775d76e0c21a651a50b13
Richard Liu <richy.liu.2002@gmail.com>
2016-04-22 01:34:00 +0000

    update

--- singulr.js
+++ singulr.js
@@ -1,21 +1,57 @@
 /* global $ */
 
+
 (function () {
+    var history = [];
+    var options = {
+        test: true
+    };
+    
+    
+    
     window.Singulr = function (userOptions) {
-        // Default options
-        var options = {
-            test: true
-        }
-        
         // Add user options
         for (var option in userOptions) {
             options[option] = userOptions[option];
         }
         
+        
+        
+        // load home page
+        loadPage('home.html');
+        
+        
+        
+        // bind event handlers
         $('a').click(function(event) {
             event.preventDefault();
+            var page = $(this).attr('href');
             
-            $('#page').load($(this).attr('href'));
+            loadPage(page);
+            // push to history
+            history.push(page);
+            
+            console.log(history);
         });
+        
+        
+        $('#back-arrow').click(function() {
+            goBack();
+        });
+    };
+    
+    
+    
+    
+    function loadPage(page) {
+        $('#page').load(page);
+    }
+    
+    
+    function goBack() {
+        if (!history.length < 1) {
+            var page = history.pop();
+            loadPage(page);
+        }
     }
 })();
